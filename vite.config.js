ec3902e18d697ede021e48e5f42369e65889f8d8
downred <redonqerimi@gmail.com>
2023-02-01 01:09:10 +0100

    restore vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue2 from "@vitejs/plugin-vue2";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"./student-information-system-final/",
+  base:"/student-information-system-final/",
   plugins: [
     vue2(),
     legacy({
